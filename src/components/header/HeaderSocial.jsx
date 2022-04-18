82c963bd9ebe6ff5823d3934df2bf17377ef8462
Haru-777 <linahoyos14@gmail.com>
2022-04-18 18:57:22 -0500

    first commit

---
+++ src/components/header/HeaderSocial.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {FiLinkedin} from 'react-icons/fi'
+import {FiGithub} from 'react-icons/fi'
+
+const HeaderSocial = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://www.linkedin.com/in/lina-daney-hoyos-chicangana-258a721b8/' target="_blank"><FiLinkedin/></a>
+        <a href='https://github.com/Haru-777' target="_blank"><FiGithub/></a>
+        <a href='https://github.com/Haru0218' target="_blank"><FiGithub/></a>
+        </div>
+  )
+}
+
+export default HeaderSocial
