13fd12737e016a06f5799930c9dfff69d45c2c3b
Haru-777 <linahoyos14@gmail.com>
2022-04-18 20:09:24 -0500

    Feat:finish nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,24 @@
 import './nav.css'
 import React from 'react'
- 
+import {GoHome} from 'react-icons/go'
+import {SiAboutdotme} from 'react-icons/si'
+import {MdWorkspacesOutline} from 'react-icons/md'
+import {MdOutlineDesignServices} from 'react-icons/md'
+import {IoIosContact} from 'react-icons/io'
+import { useState } from 'react'
+
+
+
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : '' }><GoHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : '' } ><SiAboutdotme/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : '' } ><MdWorkspacesOutline/></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : '' } ><MdOutlineDesignServices/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : '' } ><IoIosContact/></a>
+    </nav>
   )
 }
 
